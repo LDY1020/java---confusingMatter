1b242eb28cb63b916488228fd7214c15ab2b6833
LDY1020 <164770190+LDY1020@users.noreply.github.com>
2025-11-04 12:26:04 +0900

    입력받아서 점수 출력하기 - 연습 더 필요

--- Student.java
+++ Student.java
@@ -1,20 +1,28 @@
-package javamatter.ex1;
+package test2.ex7;
 
 public class Student {
-	
-	private String name;
-	private int score;
-	
-	public Student(String name , int score){
-		this.name = name;
-		this.score = score;
-	}
-	
-	public int getScore() {
-		return score;
-	}
-	public String getName() {
-		return name;
-	}
 
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
--- StudentMain.java
+++ StudentMain.java
@@ -1,29 +1,46 @@
-package javamatter.ex1;
+package test2.ex7;
+
+import java.util.Scanner;
 
 public class StudentMain {
 
-	public static void main(String[] args) {
-		Student[] s = new Student[3];
-		
-		s[0] = new Student("도윤" , 90);
-		s[1] = new Student("영희" , 80);
-		s[2] = new Student("철수" , 70);
-		
-		int sum = 0;
-		int maxScore = 0;
-		String maxName = "";
-		for(Student s1 : s){
-			sum += s1.getScore();
-			if(maxScore < s1.getScore()) {
-				maxScore = s1.getScore();
-				maxName = s1.getName();
-				}
-		}
-		double avg = (double)sum / s.length;
-		System.out.println("최고점 학생 : " + maxName);
-		System.out.println("학생 평균 점수 : " + avg);
-		
+    public static void main(String[] args) {
+        Student[] students = new Student[5];
+        Scanner sc = new Scanner(System.in);
 
-	}
+        for(int i = 0; i < students.length; i++){
+            System.out.println("학생 이름을 입력하세요 : ");
+            String name = sc.nextLine();
+            System.out.println("점수를 입력하세요 : ");
+            int score = sc.nextInt();
+            sc.nextLine();
+            students[i] = new Student(name,score);
+        }
 
+        System.out.println("===결과===");
+        int sum = 0;
+        for(int i = 0; i < students.length; i++){
+            sum += students[i].getScore();
+        }
+        double avg = (double) sum/ students.length;
+        System.out.println("평균 점수 : " + avg);
+
+        int maxScore = students[0].getScore();
+        String maxName = students[0].getName();
+        int minScore = students[0].getScore();
+        String minName = students[0].getName();
+
+        for (int i = 1; i < students.length; i++) {
+            if (students[i].getScore() > maxScore) {
+                maxScore = students[i].getScore();
+                maxName = students[i].getName();
+            }
+            if (students[i].getScore() < minScore) {
+                minScore = students[i].getScore();
+                minName = students[i].getName();
+            }
+        }
+        System.out.println("최고점 : " + maxScore);
+        System.out.println("최저점 : " + minScore);
+    }
 }
